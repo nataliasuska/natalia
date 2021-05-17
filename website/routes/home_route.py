8f03f96fe1708daae26f41c06cba4f6946968b9e
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-05-17 10:13:22 -0400

    Revert "form added"
    This reverts commit 3cd17e4ce1d288b8c71c12d3fea1e6506627662e.

--- website/routes/home_route.py
+++ website/routes/home_route.py
@@ -1,37 +1,9 @@
-from flask import Blueprint, render_template, Flask, request, flash
-from form import ContactForm
-from flask_mail import Mail, Message
-import smtplib
-from dotenv import load_dotenv
-load_dotenv()
-
-EMAIL = os.environ.get("EMAIL")
-PASS = os.environ.get("PASS")
-
+from flask import Blueprint, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
 @home_routes.route("/home")
-def home():
-    if request.method == "POST":
-        data = request.form
-        data = request.form
-        send_email(data["name"], data["email"], data["subject"], data["message"])
-        return render_template("home.html", msg_sent=True)
-    return render_template("home.html", msg_sent=False)
-
-
-def send_email(name, email, phone, message):
-    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nSubject: {subject}\nMessage:{message}"
-    with smtplib.SMTP("smtp.zoho.eu") as connection:
-        connection.starttls()
-        connection.login(EMAIL, PASS)
-        connection.sendmail(EMAIL, EMAIL, email_message)
-
-if __name__=="__main__":
-    home_routes.run(debug=True)
-
 def index():
     print("HOME")
     return render_template("home.html")
