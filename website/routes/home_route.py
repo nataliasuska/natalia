1f5f6056b88b4eaae485bde339c6c741f81a3c2d
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2024-01-30 11:31:17 +0100

    update 27

--- website/routes/home_route.py
+++ website/routes/home_route.py
@@ -11,6 +11,11 @@ def index():
     print("HOME")
     return render_template("home.html")
 
+@home_routes.route("/photography")
+def photography():
+    print("PHOTOGRAPHY")
+    return render_template("photography.html")
+
 @home_routes.route("/paintings")
 def paintings():
     print("PAINTINGS")
@@ -20,11 +25,6 @@ def paintings():
 def drawings():
     print("DRAWINGS")
     return render_template("drawings.html")
-
-@home_routes.route("/photography")
-def photography():
-    print("PHOTOGRAPHY")
-    return render_template("photography.html")
 
 @home_routes.route('/result',methods = ['POST', 'GET'])
 def result():
